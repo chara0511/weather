f3f2e1f05a71e73949dde41430d236ddc423abf2
Chara- <escor05_11@hotmail.com>
2020-07-01 15:19:48 -0500

    ready

--- src/App.js
+++ src/App.js
@@ -25,15 +25,13 @@ function App() {
   useEffect(() => {
     const consultAPI = async () => {
       if (consult) {
-        const appId = "your appId";
+        const appId = "0c0b7d1bcc3e964ca6ef54c3aa586a5d";
 
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${appId}`;
 
         const request = await fetch(url);
 
         const result = await request.json();
-
-        console.log(result);
 
         setResult(result);
 
